6a01cf732f66e049b4ab3707ea3c6cb022da3e52
Alyan-khattak <alyankhattake@gmail.com>
2026-09-11 21:57:40 +0500

    Made folder Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="imdb_rnn",
+    version="0.0.1",
+    author="Alyan Khattak",
+    author_email="your@email.com",
+    packages=find_packages(),
+    install_requires=[]
+)
